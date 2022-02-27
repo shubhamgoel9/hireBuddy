aa973c2fbfee38f725d0e8464bbf0ad1b6c943a5
Pritesh Pathak <pritesh.pathak@salesforce.com>
2022-02-27 17:28:11 +0530

    Error correction in main screen

--- force-app/main/default/lwc/eventItems/eventItems.js
+++ force-app/main/default/lwc/eventItems/eventItems.js
@@ -2,11 +2,31 @@ import { LightningElement, wire, api, track } from "lwc";
 import getAllEventItems from '@salesforce/apex/EventItemsController.getAllEventItem'
 import getEventItem from '@salesforce/apex/EventItemsController.getEventItem'
 export default class EventItems extends LightningElement {
+    @track selectedEventId;
     @track isModalOpen = false;
-    openModal() {
+    @track eventItemRecords;
+    eventId='a008c00000YUBTYAA5';
+    @wire(getAllEventItems, {eventId: '$eventId'}) eventItems;
+    openModal(event) {
         // to open modal set isModalOpen tarck value as true
         this.isModalOpen = true;
+        this.selectedEventId=event.target.dataset.id;
+        console.log('pp1: '+event.target.dataset.id);
+
+        getEventItem({eventItemId:this.selectedEventId})
+		.then(result => {
+			this.eventItemRecords = result;
+			this.error = undefined;
+            console.log('PPa: '+this.eventItemRecords + 'Result :: '+result);
+
+		})
+		.catch(error => {
+			this.error = error;
+			this.eventItemRecords = undefined;
+		})
+        
     }
+
     closeModal() {
         // to close modal set isModalOpen tarck value as false
         this.isModalOpen = false;
@@ -16,10 +36,21 @@ export default class EventItems extends LightningElement {
         //Add your code to call apex method or do some processing
         this.isModalOpen = false;
     }
-    eventId='a008c00000YUBTYAA5';
-    @wire(getAllEventItems, {eventId: '$eventId'}) eventItems;
-
-    eventItemId='';
-    @wire(getEventItem, {eventItemId:'$eventItemId'}) eventItemRecord;
+    @track isModal2Open = false;
+    openModal2() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModal2Open = true;
+    }
+    closeModal2() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModal2Open = false;
+    }
+    submitDetails2() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModal2Open = false;
+    }
+    /*eventItemId=this.selectedEventId;
+    @wire(getEventItem, {eventItemId:'$eventItemId'}) eventItemRecord;*/
     
 }
